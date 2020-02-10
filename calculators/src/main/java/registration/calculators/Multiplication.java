28c2f463ae1db436f17ae3d8bbfade46f0646a5d
balamuralibalu <nammivenkatabalamurali@gmail.com>
2020-02-10 21:42:52 +0530

    commit

---
+++ calculators/src/main/java/registration/calculators/Multiplication.java
@@ -0,0 +1,15 @@
+package registration.calculators;
+
+public class Multiplication {
+	float operand1;
+	float operand2;
+	Multiplication(float operand1,float operand2)
+	{
+		this.operand1=operand1;
+		this.operand2=operand2;
+	}
+	public float Mul(float A,float B)
+	{
+		return(A*B);
+	}
+}
